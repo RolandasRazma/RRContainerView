a869460b486d8f5e4055177136680ccf5fd94a4a
Rolandas Razma <rolandas@razma.lt>
2013-05-14 22:59:48 +0100

    clearifyed license

--- RRContainerView/RRContainerView/Classes/Module/RRStoryboardEmbedSegue.m
+++ RRContainerView/RRContainerView/Classes/Module/RRStoryboardEmbedSegue.m
@@ -1,9 +1,25 @@
 //
-//  UIStoryboardEmbedSegueTemplate.m
+//  RRStoryboardEmbedSegue.m
 //  RRContainerView
 //
-//  Created by Rolandas Razma on 14/05/2013.
-//  Copyright (c) 2013 UD7. All rights reserved.
+//  Copyright (c) 2013 Rolandas Razma <rolandas@razma.lt>
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 //
 
 #import "RRStoryboardEmbedSegue.h"
@@ -25,7 +41,7 @@
 + (void)load {
     
     // here I'm checking for NSLayoutConstraint because UIStoryboardEmbedSegueTemplate is private in iOS6
-    // keep in mind that I'm NOT using any private classes here
+    // keep in mind that I'm NOT using any private classes here as UIStoryboardEmbedSegueTemplate doesn't exist in iOS5
     if( !NSClassFromString(@"NSLayoutConstraint") ){
         objc_registerClassPair(objc_allocateClassPair([RRStoryboardEmbedSegue class], "UIStoryboardEmbedSegueTemplate", 0));
     }
